8974670fb6447213985f50857d41e250f587a161
Godwill Exercices Solutions <afolabigodwill24@gmail.com>
2023-02-19 23:48:33 -0600

    adding main WordCount class

---
+++ WordCount.java
@@ -0,0 +1,20 @@
+/*
+ * WordCount.java
+ * Contains the main to create the WordCount application.
+ */
+
+ /**
+ * Driver class for the Temperature application.
+ * @author Godwill Afolabi
+ */
+
+public class WordCount {
+
+    /**
+     * Creates a WordCountGUI object and makes it visible.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Controller theApplication = new Controller();
+    }
+}
